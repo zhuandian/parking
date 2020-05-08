e8689e736fa05d309c1d2fb0c9f16b40b987c9cd
xiedong <xieguodong@chinanews.com.cn>
2020-05-08 11:37:00 +0800

    修改高版本android手机网络权限问题

--- app/src/main/java/com/example/toby/baimap/MainActivity.java
+++ app/src/main/java/com/example/toby/baimap/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.toby.baimap;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,5 +45,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,MineActivity.class));
             }
         });
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            ArrayList<String> permissions = new ArrayList<>();
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+
+            if (permissions.size() != 0) {
+                requestPermissionsForM(permissions);
+            }
+        }
+    }
+
+
+    private void requestPermissionsForM(final ArrayList<String> per) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(per.toArray(new String[per.size()]), 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
